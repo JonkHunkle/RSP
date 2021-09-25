1693c8ff7c9442bd302c3d46622b896b745e6dac
JonkHunkle <dczerwinski@ymail.com>
2021-09-25 15:31:12 -0700

    amending RSP project

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-// focus on what the user choose and compare with computer choice,
-// rules of R,P,S game
-// user determines wins:
-// P beats R
-// R beats S
-// S beats P
-//determines ties if computer picks and user picks are the same
-// computer pick- generate a randon number, collates to a position of the array
-// array has R, P, S
-// user pick from prompt
-//otherwise is a loss
-//create wins variable, losses variable and ties variable, they keep user status from playing the game
+//I want to play "rock, paper scissors"
 var wins = 0;
 var loss = 0;
 var tie = 0;
-
+//choice
 var chosenLetter = ["R", "S", "P"];
+//play!
 function play() {
+  //computer picks random number between 0 and 2
   var arrayIndex = Math.floor(Math.random() * 3);
 
   var computerPick = chosenLetter[arrayIndex];
-
+  // get player pick and change to upper case
   var userPick = prompt("please enter R, P, or S");
 
   userPick = userPick.toUpperCase();
 
+  //LOGIC!! if a user picks p, r, or s, you continue, otherwise it is invalid.
   if (userPick === "R" || userPick === "P" || userPick === "S") {
+    //Setting win, tie, and loss conditions and logging the results in the console
     if (
+      //win
       (userPick === "P" && computerPick === "R") ||
       (userPick === "R" && computerPick === "S") ||
       (userPick === "S" && computerPick === "P")
@@ -33,24 +27,34 @@ function play() {
       wins++;
       alert("you chose: " + userPick + "Computer chose: " + computerPick);
       alert("you win!");
+      console.log("wins: " + wins + "  losses: " + loss + "  ties: " + tie);
     } else if (userPick === computerPick) {
+      //tie
       alert("you chose: " + userPick + "Computer chose: " + computerPick);
       alert("you tied!");
       tie++;
+      console.log("wins: " + wins + "  losses: " + loss + "  ties: " + tie);
     } else {
-      1;
+      //everything else=loss
       alert("you chose: " + userPick + "Computer chose: " + computerPick);
       alert("you lost!");
       loss++;
+      console.log("wins: " + wins + "  losses: " + loss + "  ties: " + tie);
     }
+    //then we see if you want to play again!
     var isPlayagain = confirm("play again?");
     if (isPlayagain) {
+      //if so, RUN THE FUNSCTION AGAIN!
       play();
     } else {
+      //otherwise, show your stats!
       alert("wins: " + wins + "losses: " + loss + "ties: " + tie);
     }
   } else {
     alert("invalid");
+    play();
   }
 }
 play();
+
+//GOOD LUCK :D
